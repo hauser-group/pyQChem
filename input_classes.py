1038cc1a624ecf71a7681973d12768e090dd8474
Matthew Goldey <matthew.goldey@gmail.com>
2014-02-27 13:28:51 -0800

    Adding undocumented rem workaround

--- input_classes.py
+++ input_classes.py
@@ -6450,8 +6450,26 @@ By default, all integrals are used in decomposed format allowing significant red
         else:
             self.dict_of_keywords["DIRECT_RI"]=value.lower()
 
-
     # ------------------------ End of keyword list ----------------------------
+    
+    # ------------------------ UNDOCUMENT KEYWORDS CODE -----------------------
+    
+    def undoc(self,undoc_str,value="show"):
+        '''\nFor rem values without documenation herein, please use the undoc keyword and add them manually'''
+        if value == "":
+            if undoc_str in self.dict_of_keywords:
+                del self.dict_of_keywords[undoc_str]
+                print "Keyword removed."
+        elif value == "show":
+            if undoc_str in self.dict_of_keywords:
+                return self.dict_of_keywords[undoc_str]
+            else:
+                print "Value not set."
+        else:
+            self.dict_of_keywords[undoc_str]=value.lower()
+    
+    # ------------------------ END OF UNDOCUMENT KEYWORDS CODE ----------------
+    
         
     def add(self,keyword,value):
         self.dict_of_keywords[keyword.upper()]=value.lower()
