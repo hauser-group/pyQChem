54d4db4d1762c8c82f638c9379072ecb3b951cd5
ralf-meyer <meyer.ralf@yahoo.com>
2017-11-29 12:32:10 +0100

    CHG: restructure of read nuclear orientation

--- output_classes.py
+++ output_classes.py
@@ -527,11 +527,13 @@ class _outputfile(object):
             if switch == 1 and "Standard Nuclear Orientation" in line:
                 switch = 2
                 initial_cartesian = cartesian("sp job - initial geometry in standard orientation")
-            if switch == 2 and "Atom" not in line and "---" not in line and "Standard" not in line and "Repulsion" not in line:
+                continue
+            if switch == 2 and ("Repulsion" in line or "Molecular" in line):
+            	switch = 3
+            elif switch == 2 and "Atom" not in line and "---" not in line:
             	dummy = (line.split())[1:]
             	initial_cartesian.add_atom(dummy[0],dummy[1],dummy[2],dummy[3])
-            if switch == 2 and ("Repulsion" in line or "Molecular Point Group" in line):
-            	switch = 3
+            
         inputfile = _readinput(infile_content,silent)
 
         # Creating geometry objects under 'general' for convenience, final geometry will be overwritten later if different
